a7f83372e172d6848d9178faa9237d99b556a2d0
SamwithWebsite <mhaskesamadhan223@gmail.com>
2022-10-25 21:54:24 +0530

    change in models in posts

--- socialmediasite/posts/models.py
+++ socialmediasite/posts/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
+form profiles.models import person
 # Create your models here.
+
+
+
+class post(models.Model):
+    name = models.CharField(max_length=30)
--- socialmediasite/socialmediasite/settings.py
+++ socialmediasite/socialmediasite/settings.py
@@ -125,3 +125,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'data/') # 'data' is my media folder
+MEDIA_URL = '/media/'
